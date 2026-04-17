9b8cc628a2c4864a0075ae7725c887030fdbaeee
CappieGold <144028326+CappieGold@users.noreply.github.com>
2026-04-17 13:38:41 +0000

    fix: first page

--- 0x03-asynchronous/rick_and_morty.dart
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -3,15 +3,12 @@ import 'package:http/http.dart' as http;
 
 Future<void> printRmCharacters() async {
   try {
-    String? url = 'https://rickandmortyapi.com/api/character';
-    while (url != null) {
-      final response = await http.get(Uri.parse(url));
-      Map<String, dynamic> data = jsonDecode(response.body);
-      List<dynamic> results = data['results'];
-      for (var character in results) {
-        print(character['name']);
-      }
-      url = data['info']['next'];
+    final response =
+        await http.get(Uri.parse('https://rickandmortyapi.com/api/character'));
+    Map<String, dynamic> data = jsonDecode(response.body);
+    List<dynamic> results = data['results'];
+    for (var character in results) {
+      print(character['name']);
     }
   } catch (e) {
     print('error caught: $e');
